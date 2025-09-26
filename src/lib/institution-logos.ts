207a03a111f9d216a904e5ad989bc62e8901d91b
unknown <awu200592@gmail.com>
2025-09-25 22:03:10 -0400

    added logos of institutions for ui

---
+++ src/lib/institution-logos.ts
@@ -0,0 +1,24 @@
+// Map of institution IDs to their logo URLs
+export const institutionLogos: Record<string, string> = {
+  // Wells Fargo
+  ins_127991:
+    "https://upload.wikimedia.org/wikipedia/commons/b/b3/Wells_Fargo_Bank.svg",
+  // E*TRADE
+  ins_129473: "https://upload.wikimedia.org/wikipedia/commons/2/22/ETrade.svg",
+  // Add more institutions as needed
+  ins_1:
+    "https://upload.wikimedia.org/wikipedia/commons/4/4f/Chase_Bank_logo.svg", // Chase
+  ins_2:
+    "https://upload.wikimedia.org/wikipedia/commons/2/2f/Bank_of_America_logo.svg", // Bank of America
+  ins_3: "https://upload.wikimedia.org/wikipedia/commons/0/0d/Citibank.svg", // Citibank
+  ins_5: "https://upload.wikimedia.org/wikipedia/commons/f/f4/US_Bank_logo.svg", // US Bank
+  ins_6:
+    "https://upload.wikimedia.org/wikipedia/commons/4/4b/Charles_Schwab_Corporation_logo.svg", // Charles Schwab
+  ins_7:
+    "https://upload.wikimedia.org/wikipedia/commons/8/8d/Capital_One_logo.svg", // Capital One
+  ins_8: "https://upload.wikimedia.org/wikipedia/commons/4/4d/TD_Bank_logo.svg", // TD Bank
+  ins_9:
+    "https://upload.wikimedia.org/wikipedia/commons/3/3b/Navy_Federal_Credit_Union_logo.svg", // Navy Federal
+  ins_10: "https://upload.wikimedia.org/wikipedia/commons/a/a0/USAA_logo.svg", // USAA
+  coinbase: "/coinbase.webp", // Coinbase
+};
