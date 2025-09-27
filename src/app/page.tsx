667aabffb4f1adcd5e922846248ffdc168beea51
unknown <awu200592@gmail.com>
2025-09-27 00:28:54 -0400

    installed openapi generator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react';
+import { usePlaidLink } from "react-plaid-link";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -11,6 +12,51 @@ import {
 } from "@/src/components/ui/navigation-menu"
 
 export default function LandingPage() {
+  const [linkToken, setLinkToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getToken = async () => {
+      try {
+        const response = await fetch("/api/plaid/create-link-token", {
+          method: "POST",
+        });
+        if (!response.ok) throw new Error("Failed to create link token");
+        const { link_token } = await response.json();
+        setLinkToken(link_token);
+      } catch (error) {
+        console.error("Error getting link token:", error);
+      }
+    };
+
+    if (!linkToken) {
+      console.error("Link token is null");
+      getToken();
+    }
+  }, [linkToken]);
+
+  const onSuccess = useCallback(
+    async (public_token: string) => {
+      try {
+        const response = await fetch("/api/plaid/exchange-token", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ public_token }),
+        });
+
+        if (!response.ok) throw new Error("Failed to exchange token");
+        refetch();
+      } catch (error) {
+        console.error("Error linking account:", error);
+      }
+    },
+    [refetch]
+  );
+
+  const { open, ready } = usePlaidLink({
+    token: linkToken,
+    onSuccess,
+  });
+
   return (
     <div className="min-h-screen bg-gray-50">
       
@@ -44,12 +90,13 @@ export default function LandingPage() {
       <main className="flex flex-col items-center justify-left flex-1 px-4 py-12 text-left">
         <h1 className="text-5xl font-bold mb-4"> Money Talks, </h1>
         <h1 className="text-5xl font-bold mb-4"> Dough Listens. </h1>
-        <a
-          href="#"
-          className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
-        >
-          Get Started
-        </a>
+         <button
+                onClick={() => open()}
+                disabled={!ready}
+                className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:opacity-50"
+              >
+                Connect Bank
+          </button>
       </main>
     </div>
   )
