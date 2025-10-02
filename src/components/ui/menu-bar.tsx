74981aa311735b26af75074ca7c2ccdcb57fed55
Chengtai Wu <cw4483@nyu.edu>
2025-10-02 19:39:52 -0400

    modified menu bar

--- src/components/ui/menu-bar.tsx
+++ src/components/ui/menu-bar.tsx
@@ -8,36 +8,23 @@ import { cn } from "@/src/lib/utils"
 export function MenuBar(){
     return (
          <header className="flex justify-center p-6 bg-white shadow-md">
-        
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
               <NavigationMenuTrigger>Item1</NavigationMenuTrigger>
-
-
-
                 <NavigationMenuContent>
                   <NavigationMenuLink>Link1</NavigationMenuLink>
                 </NavigationMenuContent>
-
-
-
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
-
           <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
               <NavigationMenuTrigger>Item1</NavigationMenuTrigger>
-
-
                 <NavigationMenuContent>
                   <NavigationMenuLink>Link1</NavigationMenuLink>
                 </NavigationMenuContent>
-
-
-
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
