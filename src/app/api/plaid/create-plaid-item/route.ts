9391f45e0e311412873930f85dd0c0345efcd0bd
unknown <awu200592@gmail.com>
2025-10-04 21:16:57 -0400

    transferring work

--- src/app/api/plaid/create-plaid-item/route.ts
+++ src/app/api/plaid/create-plaid-item/route.ts
@@ -12,13 +12,14 @@ export async function POST(request: Request) {
   let access_token: string;
   let item_id: string;
   try {
-  const exchangeResponse = await plaidClient.itemPublicTokenExchange({ public_token });
-  const temp_access_token = exchangeResponse.data.access_token;
-  const temp_item_id = exchangeResponse.data.item_id;
-  access_token = temp_access_token;
-  item_id = temp_item_id;
-  console.log("Public token exchanged for access token.", { item_id });
-  } catch (error) {
+    const exchangeResponse = await plaidClient.itemPublicTokenExchange({ public_token });
+    const temp_access_token = exchangeResponse.data.access_token;
+    const temp_item_id = exchangeResponse.data.item_id;
+    access_token = temp_access_token;
+    item_id = temp_item_id;
+    console.log("Public token exchanged for access token.", { item_id });
+  } 
+  catch (error) {
     console.error("Error exchanging public token:", error);
     return NextResponse.json({ error: "Failed to exchange public token" }, { status: 500 });
   }
@@ -37,15 +38,17 @@ export async function POST(request: Request) {
     });
     institution = instRes.data.institution ?? null;
     console.log(`Fetched institution details, id: ${institutionId} name: ${institution.name}`);
-  } catch (err) {
-    console.warn("Could not fetch institution details", err);
+  } 
+  catch (error) {
+    console.warn("Could not fetch institution details", error);
   }
 
   // Find if plaidItem already exists
-  const existing = await prisma.plaidItem.findUnique({ where: { itemId: item_id } });
+  const existingItem = await prisma.plaidItem.findUnique({ where: { itemId: item_id } });
+  const accountsResponse = await plaidClient.accountsGet({ access_token });
 
   // Create new plaidItem if it doesn't exist
-  if (!existing) {
+  if (!existingItem) {
     console.log("Creating new PlaidItem...");
     const newItem = await prisma.plaidItem.create({
       data: {
@@ -56,24 +59,51 @@ export async function POST(request: Request) {
         institutionLogo: institution?.logo,
       },
     });
-    const accountsResponse = await plaidClient.accountsGet({ access_token });
     for (const account of accountsResponse.data.accounts) {
-      console.log(`Creating new account: ${account.name} (${account.mask})`)
-      const newAccount = await prisma.account.create({
-        data: {
-          accountId: account.account_id,
-          name: account.name,
-          type: account.type,
-          subtype: account.subtype || null,
-          mask: account.mask || null,
-          itemId: newItem.id,
-        },
-      });
+      createAccount(account, item_id);
     }
 
-  // if it exists, update the access token
+  // If it exists, update the plaidItem
   } else {
-    // update account instead 
+    console.log("PlaidItem already exists, updating...");
+    for (const account of accountsResponse.data.accounts) {
+      // check if account exists
+      const existingAccount = await prisma.account.findUnique({ where: { accountId: account.account_id } });
+      if (existingAccount){
+        updateAccount(existingAccount, item_id);
+      }
+      else {
+        createAccount(account, item_id);
+      }
+    }
   }
+} // End of POST  
+ 
+
+// Helper functions 
+async function updateAccount(account: any, item_id: string) {
+  await prisma.account.update({
+    where: {id: account.id},
+    data: {
+      accountId: account.accountId,
+      name: account.name,
+      type: account.type,
+      subtype: account.subtype || null,
+      mask: account.mask || null,
+    }
+  })
 }
 
+async function createAccount(account: any, item_id: string) {
+  console.log(`Creating new account: ${account.name} (${account.mask})`)
+  const newAccount = await prisma.account.create({
+    data: {
+      accountId: account.account_id,
+      name: account.name,
+      type: account.type,
+      subtype: account.subtype || null,
+      mask: account.mask || null,
+      itemId: item_id,
+    },
+  });
+}
