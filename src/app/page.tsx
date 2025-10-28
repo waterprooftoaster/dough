3a00a6496f0dcb70e588a1a85a3cda920b59852f
Chengtai Wu <cw4483@nyu.edu>
2025-10-28 19:16:12 -0400

    small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function LandingPage() {
 				className="flex flex-col items-center justify-center h-screen bg-cover bg-center"
 				style={{ backgroundImage: "url(/background.png)" }}
 			>
-				<h1 className="text-9xl sm:text-9xl tracking-tight font-spectral font-normal">
+				<h1 className="text-9xl sm:text-9xl tracking-tight  font-spectral font-normal">
 					Money Talks,
 				</h1>
 				<h1 className="text-9xl sm:text-9xl tracking-tight font-spectral font-normal mt-2">
