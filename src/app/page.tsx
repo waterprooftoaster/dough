f4e35a2d51e8130ef9cc547781532e90efea4f3e
unknown <awu200592@gmail.com>
2025-10-28 22:55:30 -0400

    small menubar changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function LandingPage() {
 			<MenuBar />
 			<section
 				className="flex flex-col items-center justify-center h-screen bg-cover bg-center"
-				style={{ backgroundImage: "url(/background.png)" }}
+				style={{ backgroundImage: "url(/tempBackground.jpg)" }}
 			>
 				<h1 className="text-9xl sm:text-9xl tracking-tight  font-spectral font-normal">
 					Money Talks,
--- src/components/menu-bar.tsx
+++ src/components/menu-bar.tsx
@@ -10,8 +10,8 @@ import { cn } from "@/src/lib/utils"
 
 export function MenuBar() {
   return (
-    <header className="flex fixed shadow-md bg-background z-50 top-0 left-0 right-0 p-3">
-      <NavigationMenu viewport={false}>
+    <header className="fixed flex items-center bg-black/20 backdrop-blur-lg top-0 left-0 right-0 z-50 p-3">
+      <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
 
@@ -184,7 +184,9 @@ function NavigationMenuItem({
 }
 
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-accent data-[state=open]:text-accent-foreground data-[state=open]:focus:bg-accent data-[state=open]:bg-accent/50 focus-visible:ring-ring/50 outline-none transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1"
+  // "group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-accent data-[state=open]:text-accent-foreground data-[state=open]:focus:bg-accent data-[state=open]:bg-accent/50 focus-visible:ring-ring/50 outline-none transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1"
+  "group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-accent data-[state=open]:text-accent-foreground data-[state=open]:focus:bg-accent data-[state=open]:bg-accent/50 focus-visible:ring-ring/50 outline-none transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1"
+
 )
 
 function NavigationMenuTrigger({
