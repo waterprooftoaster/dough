eab226d4108a922296c388a714669bb224b996c2
unknown <awu200592@gmail.com>
2025-11-04 16:11:04 -0500

    ui changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,5 @@
-
-
 import Link from "next/link"
-
+import { useIsMobile } from "@/src/hooks/use-mobile"
 import {
   NavigationMenu,
   NavigationMenuList,
@@ -12,125 +10,14 @@ import {
   NavigationMenuIndicator,
   NavigationMenuViewport,
   navigationMenuTriggerStyle,
-} from "@/src/components/ui/menu-bar"
+} from "@/src/components/ui/navigation-menu"
 
 export function Header() {
+  const isMobile = useIsMobile();
   return (
     <header className="flex items-center bg-background top-0 left-0 right-0 p-6">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
+      <NavigationMenu viewport={isMobile}>
 
-            {/* Item 1 */}
-            <NavigationMenuTrigger>Item 1</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid gap-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                <li className="row-span-3">
-                  <NavigationMenuLink asChild>
-                    <a
-                      className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-linear-to-b p-6 no-underline outline-hidden select-none focus:shadow-md"
-                      href="/"
-                    >
-                      <div className="mt-4 mb-2 text-lg font-medium">
-                        shadcn/ui
-                      </div>
-                      <p className="text-muted-foreground text-sm leading-tight">
-                        Beautifully designed components built with Tailwind CSS.
-                      </p>
-                    </a>
-                  </NavigationMenuLink>
-                </li>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-
-          {/* Item 2 */}
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item 2</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-
-          {/* Item 3 */}
-          <NavigationMenuItem>
-            <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-              <Link href="">Item 3</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-
-          {/* Item 4 */}
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item 4</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[300px] gap-4">
-                <li>
-                  <NavigationMenuLink asChild>
-                    <Link href="#">
-                      <div className="font-medium">Components</div>
-                      <div className="text-muted-foreground">
-                        Browse all components in the library.
-                      </div>
-                    </Link>
-                  </NavigationMenuLink>
-                  <NavigationMenuLink asChild>
-                    <Link href="#">
-                      <div className="font-medium">Documentation</div>
-                      <div className="text-muted-foreground">
-                        Learn how to use the library.
-                      </div>
-                    </Link>
-                  </NavigationMenuLink>
-                  <NavigationMenuLink asChild>
-                    <Link href="#">
-                      <div className="font-medium">Blog</div>
-                      <div className="text-muted-foreground">
-                        Read our latest blog posts.
-                      </div>
-                    </Link>
-                  </NavigationMenuLink>
-                </li>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-
-          {/* Item 5 */}
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item 5</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[200px] gap-4">
-                <li>
-                  <NavigationMenuLink asChild>
-                    <Link href="#">Components</Link>
-                  </NavigationMenuLink>
-                  <NavigationMenuLink asChild>
-                    <Link href="#">Documentation</Link>
-                  </NavigationMenuLink>
-                  <NavigationMenuLink asChild>
-                    <Link href="#">Blocks</Link>
-                  </NavigationMenuLink>
-                </li>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-
-          {/* Item 6 */}
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item 6</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[200px] gap-4">
-                <li>
-                  <NavigationMenuLink asChild>
-                    <Link href="#" className="flex-row items-center gap-2">
-                      Done
-                    </Link>
-                  </NavigationMenuLink>
-                </li>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
       </NavigationMenu>
     </header>
   )
